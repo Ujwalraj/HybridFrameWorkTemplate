69ce9b88ff6ec3c1e7be1193c36dd07455a1d827
UjwalRaj Shekar <ujwalraj.shekar@gmail.com>
2018-06-20 15:22:56 +0530

    Added log4j

--- src/main/java/testBase/TestBase.java
+++ src/main/java/testBase/TestBase.java
@@ -1,8 +1,15 @@
 package testBase;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -14,29 +21,51 @@ import org.testng.annotations.Parameters;
 import com.aventstack.extentreports.ExtentTest;
 
 public class TestBase {
+	
+	public static final Logger log = Logger.getLogger(TestBase.class.getName());
 
 	public static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<RemoteWebDriver>();
 
 	public static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<ExtentTest>();
+	
+	public Properties OR = new Properties();
 
 	public WebDriver getDriver() {
 		return driver.get();
 	}
 
+	public void loadData() throws IOException {
+		File file = new File(System.getProperty("user.dir") + "\\src\\main\\resources\\config\\config.properties");
+		FileInputStream f = new FileInputStream(file);
+		OR.load(f);
+		log.info("Loaded file :"+f);
+
+	}
+	
 	@BeforeTest
 	@Parameters(value = { "URL", "Browser", "RemoteUrl" })
-	public void initialSetup(String url, String browser, String remoteUrl) throws MalformedURLException {
+	public void initialSetup(String url, String browser, String remoteUrl) throws MalformedURLException,IOException {
+		loadData();
+		log.info("Loading files");
+		String log4jConfPath = "log4j.properties";
+		PropertyConfigurator.configure(log4jConfPath);
+		log.info("Log 4j properties configured. ");
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 		desiredCapabilities.setBrowserName(browser);
+		log.info("Browser used for execution is :"+browser);
 		desiredCapabilities.setJavascriptEnabled(true);
 		desiredCapabilities.setPlatform(Platform.ANY);
 		driver.set(new RemoteWebDriver(new URL(remoteUrl), desiredCapabilities));
 		getDriver().get(url);
+		log.info("Navigating to the URL :"+url);
+		getDriver().manage().deleteAllCookies();
 		getDriver().manage().window().maximize();
+		getDriver().manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 	}
 
 	@AfterMethod
 	public void closeBrowser() {
+		log.info("Closing the browser. ");
 		getDriver().quit();
 	}
 
--- src/test/java/testMethods/GoogleTest.java
+++ src/test/java/testMethods/GoogleTest.java
@@ -1,14 +1,21 @@
 package testMethods;
 
+import java.util.logging.Logger;
+
 import org.testng.annotations.Test;
 
 import initializer.PageInitializer;
 
 public class GoogleTest extends PageInitializer {
+	
+	public Logger log = Logger.getLogger(GoogleTest.class.getName());
 
 	@Test
 	public void testing1() throws InterruptedException {
+		log.info("Test case started");
 		testPage().assertTitle("Google");
+		log.info("Test case Title verified");
+		log.info(" Test Case ended");
 	}
 
 }
